40cb43a851a6ae2819d68dcccafdcd349f4b9a69
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-03-15 20:10:12 +0000

    refactor

--- spinner2.js
+++ spinner2.js
@@ -1,4 +1,5 @@
-for (let i = 0; i < 8; i++) {
+let spins = 2
+for (let i = 0; i < spins * 4; i++) {
   let spin = ['\r|   ','\r/   ','\r-   ','\r\\   '];
   setTimeout(() => { process.stdout.write(spin[i % 4]); }, i * 250);
 }
